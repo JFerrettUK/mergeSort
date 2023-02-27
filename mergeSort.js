284796065d14efde0049c12dc865d42c7f108b87
JFerrettUK <jferrettuk@gmail.com>
2023-02-27 09:42:35 +0000

    merge js amend

--- mergeSort.js
+++ mergeSort.js
@@ -21,39 +21,30 @@ console.log(mergeSort(array, low, high))
 function merge(a, b) {
     let c = []
 
-    while (a.length & b.length > 0) {
-        console.log("while activated")
-        console.log(a.length + " " + " " + b.length + " > 0 checks length")
-        console.log(a + " is a")
-        console.log(b + " is b")
-
+    if (a.length & b.length > 0) {
         if (a[0] > b[0]) {
             console.log(a[0] + " > " + b[0])
             c.push(b[0])
             b.shift();
         } else {
-            console.log(a + " a before")
             console.log(a[0] + " < " + b[0])
             c.push(a[0])
             a.shift();
-            console.log(a + " a after")
         }
     }
 
-    while (a.length > 0) {
+    if (a.length > 0) {
         console.log("a has elements so push to end of c") 
         c.push(a[0])
         a.shift();
     }
 
-    while (b.length > 0) {
+    if (b.length > 0) {
         console.log("b has elements so push to end of c") 
         c.push(b[0])
         b.shift();
     }
 
-    console.log(c + " is a")
-    console.log(c + " is b")
     console.log(c + " is c")
     return c
     //at this point, a and b should be empty
